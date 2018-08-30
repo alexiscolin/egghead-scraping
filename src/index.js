666a17b166d1b13a6b7b7c0568f5f10bae62feef
Alexis Colin <nesquimo@gmail.com>
2018-08-30 17:37:20 +0200

    Enable headless mode

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const URL = process.argv[2]; // get URL to scrape
   const EGGHEAD_VIDEO_BTN = '#App-react-component > div > div:nth-child(2) > section > div > div.relative.w-100 > section.w-100.pt2.pb3.pt0-l.pb0-l.css-ic4s7x > div > div.w-100.w-70-l.false > div.relative.items-center.justify-between.pv2.flex.css-1kldd4v > div.flex.items-center > div.ml3.dn.db-l > div';
 
   // Puppeteer action
-  const browser = await puppeteer.launch({ headless: false }); // no headless because of chromium bug https://bugs.chromium.org/p/chromium/issues/detail?id=696481
+  const browser = await puppeteer.launch({ headless: true }); // no headless because of chromium bug https://bugs.chromium.org/p/chromium/issues/detail?id=696481
   const page = await browser.newPage();
   await page.setViewport({ width: 1200, height: 700 })
   await page.goto('https://egghead.io/users/sign_in');
