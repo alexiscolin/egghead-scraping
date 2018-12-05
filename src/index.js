e75623758180ea8d14d612d307b14e819accb67b
Alexis Colin <nesquimo@gmail.com>
2018-12-05 11:38:32 +0100

    Change title selector

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const URL = process.argv[2]; // get URL to scrape
   const EGGHEAD_VIDEO_BTN = '#App-react-component > div > div:nth-child(2) > section > div > div.relative.w-100 > section.w-100.pt2.pb3.pt0-l.pb0-l.css-ic4s7x > div > div.w-100.w-70-l.false > div.relative.items-center.justify-between.pv2.flex.css-1kldd4v > div.flex.items-center > div.ml3.dn.db-l > div';
 
   // Puppeteer action
-  const browser = await puppeteer.launch({ headless: true }); // no headless because of chromium bug https://bugs.chromium.org/p/chromium/issues/detail?id=696481
+  const browser = await puppeteer.launch({ headless: false }); // no headless because of chromium bug https://bugs.chromium.org/p/chromium/issues/detail?id=696481
   const page = await browser.newPage();
   await page.setViewport({ width: 1200, height: 700 })
   await page.goto('https://egghead.io/users/sign_in');
@@ -32,7 +32,7 @@ const URL = process.argv[2]; // get URL to scrape
 
   // Get course title and create folder
   const courseTitle = await page.evaluate(() => {
-    const titlePath = '#App-react-component > div > div.flex.flex-column.bg-base-secondary > div > div.bg-white.pb5-ns.pb4-m.pb4 > div > div.cf.pt3.flex.flex-row-l.flex-column > div.fl.w-100.w-100-m.w-50-ns.pa2.order-2.order-0-l.flex.false > div > div.flex-none.flex-m.flex-column.tc.tl-ns > span';
+    const titlePath = '#App-react-component > div > div.flex.flex-column.bg-base-secondary > div > div.bg-white.pb5-ns.pb4-m.pb4 > div > div.cf.pt3.flex.flex-row-l.flex-column > div.fl.w-100.w-100-m.w-60-ns.pa2.order-2.order-0-l.flex.false > div > div.flex-none.flex-m.flex-column.tc.tl-ns > h1';
     return document.querySelector(titlePath).innerText;
   });
 
